4d3d975281e6848fccc2df6c4f5d8a945fe8b885
JeeveshSinghroa <jsinghroa@gmail.com>
2023-10-03 15:40:23 +0530

    feat: uploading solution for sorting 0, 1, 2 problem

---
+++ src/SortColors.java
@@ -0,0 +1,23 @@
+class SortColors {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int low = 0, mid = 0, high = n-1;
+        int temp;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if(nums[mid] ==1) {
+                mid++;
+            } else {
+                temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
